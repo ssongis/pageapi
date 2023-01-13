773b341c97da7d962e5c465d22cbcb8c52cbdca6
ssongis <thddltmf97@naver.com>
2023-01-13 09:58:39 +0900

    #API230113

---
+++ src/main/java/com/example/apibasic/ApibasicApplication.java
@@ -0,0 +1,17 @@
+package com.example.apibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibasicApplication {
+
+	public static void main(String[] args) {
+		//메롱메롱
+		int a = 10;
+		System.out.println(a);
+		SpringApplication.run(ApibasicApplication.class, args);
+
+	}
+
+}
